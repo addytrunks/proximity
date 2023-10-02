be847d1478d6b7076c85e91a8512ebc24ce64ce3
Adhithya Srivatsan <addytrunks@gmail.com>
2023-10-02 13:10:03 +0530

    fix: render user image

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,28 +1,12 @@
-"use client";
-
 import Image from "next/image";
-import Avatar from "react-avatar";
 import React from "react";
-import { useUser } from "@clerk/nextjs";
 
 interface UserAvatarProps {
-  src?: string;
+  src: string;
 }
 
 const UserAvatar = ({ src }: UserAvatarProps) => {
-  const { user } = useUser();
 
-  if (!src)
-    return (
-      <div className="relative flex h-20 w-20 shrink-0 overflow-hidden rounded-full">
-        <Image
-          src={user?.imageUrl!}
-          className="aspect-square h-full w-full"
-          fill
-          alt="avatar"
-        />
-      </div>
-    );
   return (
     <div className="relative flex h-20 w-20 shrink-0 overflow-hidden rounded-full">
       <Image
