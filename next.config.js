53a13b21c12f9bc1dd7e3a1112d088e290e152aa
Adhithya Srivatsan <addytrunks@gmail.com>
2023-10-02 13:07:11 +0530

    MISC

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:['utfs.io','img.clerk.com']
+    }
+}
 
 module.exports = nextConfig
