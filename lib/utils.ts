f347fb823ba3629dbcdd6fbe27f5b1b45fdd6bc3
Adhithya Srivatsan <addytrunks@gmail.com>
2023-10-04 18:39:11 +0530

    feat: haversine

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import haversine from 'haversine-distance'
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const withinRadius = (userLat:number,userLong:number,lat: number, long: number) => {
+
+  const radius = (haversine({ lat: userLat, lng: userLong }, { lat: lat, lng: long }))/1000;
+  console.log(radius)
+  if(radius < 5){
+    console.log('Within 5KM radius')
+    return true
+  }else{
+    console.log('Not within 5KM radius')
+    return false
+  }
+};
