28de713dccecf9716c64d6d0cdcf857678a9c3b3
Adhithya Srivatsan <addytrunks@gmail.com>
2023-10-07 12:45:40 +0530

    feat: own haversine formula

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,25 +1,65 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import haversine from 'haversine-distance'
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const withinRadius = (userLat:string,userLong:string,lat: string, long: string) => {
+const haversineFormula = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) => {
+  const earthRadius = 6371;
 
-  const radius = (haversine({ lat: parseFloat(userLat), lng: parseFloat(userLong) }, { lat: parseFloat(lat), lng: parseFloat(long) }))/1000;
-  if(radius < 5){
-    return true
-  }else{
-    return false
+  // Converting to radians
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) *
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  // Angular distance
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = earthRadius * c;
+  return distance;
+};
+
+export const withinRadius = (
+  userLat: string,
+  userLong: string,
+  lat: string,
+  long: string
+) => {
+  const radius = haversineFormula(
+    parseFloat(userLat),
+    parseFloat(userLong),
+    parseFloat(lat),
+    parseFloat(long)
+  );
+  if (radius < 5) {
+    return true;
+  } else {
+    return false;
   }
 };
 
-
-
-export const calculateRadius = (userLat:string,userLong:string,lat: string, long: string) => {
-
-  const radius = (haversine({ lat: parseFloat(userLat), lng: parseFloat(userLong) }, { lat: parseFloat(lat), lng: parseFloat(long) }))/1000;
-  return radius.toFixed(1)
+export const calculateRadius = (
+  userLat: string,
+  userLong: string,
+  lat: string,
+  long: string
+) => {
+  const radius = haversineFormula(
+    parseFloat(userLat),
+    parseFloat(userLong),
+    parseFloat(lat),
+    parseFloat(long)
+  );
+  return radius.toFixed(1);
 };
