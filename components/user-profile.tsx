17e8ac8b14e7cae5d82d586e7d4d7eb59ad5e096
Adhithya Srivatsan <addytrunks@gmail.com>
2023-10-07 20:11:43 +0530

    MISC

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -51,49 +51,63 @@ const UserProfile = ({ posts, profile, currUser }: UserProfileProps) => {
 
       <div className="max-w-2xl mx-auto">
         <hr className="border-gray-700 mt-5" />
-        <h2 className="font-semibold text-[#58A6FF] text-xl pt-4">Posts</h2>
-
-        {posts.map((post) => (
-          <Card key={post.id} className="w-full border-gray-700 mt-5 shadow-lg">
-            <CardHeader className="flex items-center space-x-5">
-              <div className="relative h-8 w-8">
-                <Image
-                  src={profile?.imageUrl!}
-                  alt="post_image"
-                  className="rounded-full"
-                  fill
-                />
-              </div>
-              <span className="font-semibold">
-                {profile?.id === currUser?.id
-                  ? "You"
-                  : profile?.name}
-              </span>
-              <span className="text-gray-500 text-xs">
-                {formatDistance(new Date(), new Date(post.createdAt))} ago
-              </span>
-              {profile?.id === currUser?.id && (
-                <span
-                  className="cursor-pointer text-right"
-                  onClick={() => router.push(`/post/${post.id}`)}
-                >
-                  <Edit className="w-4 h-4 text-gray-600 hover:text-gray-300" />
+        {posts.length > 0 && (
+          <h2 className="font-semibold text-[#58A6FF] text-xl pt-4">Posts</h2>
+        )}
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <Card
+              key={post.id}
+              className="w-full border-gray-700 mt-5 shadow-lg"
+            >
+              <CardHeader className="flex items-center space-x-5">
+                <div className="relative h-8 w-8">
+                  <Image
+                    src={profile?.imageUrl!}
+                    alt="post_image"
+                    className="rounded-full"
+                    fill
+                  />
+                </div>
+                <span className="font-semibold">
+                  {profile?.id === currUser?.id ? "You" : profile?.name}
                 </span>
-              )}
-            </CardHeader>
-            <CardContent>
-              <div className="relative lg:w-[450px] md:w-[400px] h-[400px] mx-auto">
-                <Image
-                  src={post?.imageUrl}
-                  fill
-                  className="rounded-md"
-                  alt="post"
-                />
-              </div>
-              <p className="text-sm pt-5">{post?.caption}</p>
-            </CardContent>
-          </Card>
-        ))}
+                <span className="text-gray-500 text-xs">
+                  {formatDistance(new Date(), new Date(post.createdAt))} ago
+                </span>
+                {profile?.id === currUser?.id && (
+                  <span
+                    className="cursor-pointer text-right"
+                    onClick={() => router.push(`/post/${post.id}`)}
+                  >
+                    <Edit className="w-4 h-4 text-gray-600 hover:text-gray-300" />
+                  </span>
+                )}
+              </CardHeader>
+              <CardContent>
+                <div className="relative lg:w-[450px] md:w-[400px] h-[400px] mx-auto">
+                  <Image
+                    src={post?.imageUrl}
+                    fill
+                    className="rounded-md"
+                    alt="post"
+                  />
+                </div>
+                <p className="text-sm pt-5">{post?.caption}</p>
+              </CardContent>
+            </Card>
+          ))}
+
+        {posts.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10">
+            <p className="text-lg font-semibold">No posts yet.</p>
+            {profile?.id === currUser?.id && (
+              <p className="text-sm text-gray-500 mt-2 cursor-pointer hover:text-gray-400" onClick={() => router.push('/post/create')}>
+                Create your first post.
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
